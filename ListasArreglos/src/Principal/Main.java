cf208e19a07b9bcd95c3bd60a6857278d41f60a8
Lokcis <lokcis@hotmail.com>
2024-02-03 16:17:18 -0500

    arreglos manera 2

--- ListasArreglos/src/Principal/Main.java
+++ ListasArreglos/src/Principal/Main.java
@@ -14,68 +14,43 @@ public class Main {
     public static void main(String[] args) {
 
         int opc, fin;
+        String[] mainArray, arrayOfArrays; 
 
         Scanner in = new Scanner(System.in);
+        
+        System.out.println("Bienvenido!\n");
 
-        System.out.println("""
-                           Bienvenido!
+        do {System.out.println("""                           
+                           Ingresa la accion que deseas realizar:
+                           
+                           1. Agregar.
+                           2. Agregar en una posicion especifica.
+                           3. Agregar todos los elementos a otro arreglo.
+                           4. Elimina el elemento en la posicion indicada.
+                           5. Elimina el primer elemento indicado.
+                           6. Eliminar espacios vacios.
+                           7. Eliminar todos los elementos del arreglo.
+                           8. Modifica el tamanio del arreglo.
+                           9. Reemplaza el elemento de la posicion indicada con otro ingresado.
+                           10. Retornar la primera ubicacion del elemento.
+                           11. Retorna el numero de elementos.
+                           12. Retorna la ultima ubicacion del elemento.
+                           13. Retorna los elementos entre los valores indicados.
+                           14. Verificar el elemento en la posicion indicada.
+                           15. Verificar si el arreglo esta vacio.
+                           16. Verificar si el elemento se encuentra en el arreglo.
+                            
+                           17. Salir.
                            """);
-
-        do {
-
-            System.out.println("""
-                               Qué deseas realizar?
-                               
-                               1. Agregar
-                               
-                               2. Modificar
-                               
-                               3. Salir
-                               
-                               """);
-
-            opc = in.nextInt();
             switch (opc) {
 
                 case 1 -> {
+                    
+                    
 
                 }
 
                 case 2 -> {
-
-                    System.out.println("\nIngresa la accion que deseas realizar:\n"
-                            + "\n"
-                            + "1. Agregar.\n"
-                            + "2. Agregar en una posicion especifica.\n"
-                            + "3. Agregar todos los elementos a otro arreglo.\n"
-                            + "4. Elimina el elemento en la posicion indicada.\n"
-                            + "5. Elimina el primer elemento indicado.\n"
-                            + "6. Eliminar espacios vacios.\n"
-                            + "7. Eliminar todos los elementos del arreglo.\n"
-                            + "8. Modifica el tamanio del arreglo.\n"
-                            + "9. Reemplaza el elemento de la posicion indicada con otro ingresado.\n"
-                            + "10. Retornar la primera ubicacion del elemento.\n"
-                            + "11. Retorna el numero de elementos.\n"
-                            + "12. Retorna la ultima ubicacion del elemento.\n"
-                            + "13. Retorna los elementos entre los valores indicados.\n"
-                            + "14. Verificar el elemento en la posicion indicada.\n"
-                            + "15. Verificar si el arreglo esta vacio.\n"
-                            + "16. Verificar si el elemento se encuentra en el arreglo.\n"
-                            + "\n"
-                            + "17. Salir.\n");
-
-                    opc = in.nextInt();
-
-                    switch (opc) {
-
-                        case 1 -> {
-
-                        }
-
-                        default -> {
-
-                        }
-                    }
 
                 }
 
