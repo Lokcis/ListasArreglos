53567ae6289a5ace44e76586f8b722aca5e76c5c
Lokcis <lokcis@hotmail.com>
2024-02-03 16:03:09 -0500

    add Arreglo

--- ListasArreglos/src/Mundo/Methods.java
+++ ListasArreglos/src/Mundo/Methods.java
@@ -6,11 +6,18 @@ package Mundo;
  */
 public class Methods {
 
-    
     /*public int add(int pos, String ele) {
         
         
         
 
     }*/
+    public String[] addArray(int size, String name) {
+
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + 1;
+        }
+
+    }
 }
