02a69ade4a496ca7eb48679a023587989433a661
Lokcis <lokcis@hotmail.com>
2024-02-03 15:54:50 -0500

    Textos y melo

--- ListasArreglos/src/Principal/Main.java
+++ ListasArreglos/src/Principal/Main.java
@@ -30,47 +30,60 @@ public class Main {
                                
                                2. Modificar
                                
+                               3. Salir
+                               
                                """);
 
             opc = in.nextInt();
-            switch (opc) {
-
-                case 1:
-
-                case 2:
-
-                default:System.out.println("Opción incorrecta");
-
-            }
-
-            System.out.println("\nIngresa la accion que deseas realizar:\n"
-                    + "\n"
-                    + "1. Agregar.\n"
-                    + "2. Agregar en una posicion especifica.\n"
-                    + "3. Agregar todos los elementos a otro arreglo.\n"
-                    + "4. Elimina el elemento en la posicion indicada.\n"
-                    + "5. Elimina el primer elemento indicado.\n"
-                    + "6. Eliminar espacios vacios.\n"
-                    + "7. Eliminar todos los elementos del arreglo.\n"
-                    + "8. Modifica el tamanio del arreglo.\n"
-                    + "9. Reemplaza el elemento de la posicion indicada con otro ingresado.\n"
-                    + "10. Retornar la primera ubicacion del elemento.\n"
-                    + "11. Retorna el numero de elementos.\n"
-                    + "12. Retorna la ultima ubicacion del elemento.\n"
-                    + "13. Retorna los elementos entre los valores indicados.\n"
-                    + "14. Verificar el elemento en la posicion indicada.\n"
-                    + "15. Verificar si el arreglo esta vacio.\n"
-                    + "16. Verificar si el elemento se encuentra en el arreglo.\n");
-
-            opc = in.nextInt();
-
             switch (opc) {
 
                 case 1 -> {
 
                 }
 
+                case 2 -> {
+
+                    System.out.println("\nIngresa la accion que deseas realizar:\n"
+                            + "\n"
+                            + "1. Agregar.\n"
+                            + "2. Agregar en una posicion especifica.\n"
+                            + "3. Agregar todos los elementos a otro arreglo.\n"
+                            + "4. Elimina el elemento en la posicion indicada.\n"
+                            + "5. Elimina el primer elemento indicado.\n"
+                            + "6. Eliminar espacios vacios.\n"
+                            + "7. Eliminar todos los elementos del arreglo.\n"
+                            + "8. Modifica el tamanio del arreglo.\n"
+                            + "9. Reemplaza el elemento de la posicion indicada con otro ingresado.\n"
+                            + "10. Retornar la primera ubicacion del elemento.\n"
+                            + "11. Retorna el numero de elementos.\n"
+                            + "12. Retorna la ultima ubicacion del elemento.\n"
+                            + "13. Retorna los elementos entre los valores indicados.\n"
+                            + "14. Verificar el elemento en la posicion indicada.\n"
+                            + "15. Verificar si el arreglo esta vacio.\n"
+                            + "16. Verificar si el elemento se encuentra en el arreglo.\n"
+                            + "\n"
+                            + "17. Salir.\n");
+
+                    opc = in.nextInt();
+
+                    switch (opc) {
+
+                        case 1 -> {
+
+                        }
+
+                        default -> {
+
+                        }
+                    }
+
+                }
+
+                default -> {
+                    System.out.println("Opción incorrecta.");
+                }
             }
+
         } while (fin == 0);
 
     }
