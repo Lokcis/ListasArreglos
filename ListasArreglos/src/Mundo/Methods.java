8d162fe786fbcc471c1265d258a6a20dc74cad8b
Lokcis <lokcis@hotmail.com>
2024-02-03 21:12:17 -0500

    Case 1 y 2 funcionales

--- ListasArreglos/src/Mundo/Methods.java
+++ ListasArreglos/src/Mundo/Methods.java
@@ -10,49 +10,30 @@ public class Methods {
 
         if (pos >= 0 && pos < array.length) {
 
-            if (array[pos] != null) {
-                
-                for (int i = array.length-1; i < pos; i--) {
-                    
-                    array[i + 1] = array[i];
-                    
+            if (array[pos] != null) {                
+                for (int i = array.length-1; i > pos; i--) {                    
+                    array[i] = array[i - 1];                    
                 }
-
                 array[pos] = ele;
-
             } else {
-
                 for (int i = 0; i < array.length; i++) {
-
                     if (array[i] == null) {
-
                         array[i] = ele;
-
                         break;
-
                     }
-
                 }
             }
         } else {
             throw new Exception("La posición ingresada es inválida. Por favor, ingresa una posición entre 0 y " + (array.length - 1));
         }
-
     }
 
     public void add(String ele, String[] array) throws Exception {
-
         for (int i = 0; i < array.length; i++) {
-
             if (array[i] == null) {
-
                 array[i] = ele;
-
                 break;
-
             }
-
-            // Si se recorre todo el arreglo y no se encuentra una posición disponible, se lanza una excepción
             if (i == array.length - 1) {
                 throw new Exception("El arreglo está lleno. No se puede agregar un elemento.");
             }
