0519d2ad3e08e4face651a162d5599b386ad7d8b
Lokcis <lokcis@hotmail.com>
2024-02-03 20:32:13 -0500

    Case 1 con posicion, falta mover numeros

--- ListasArreglos/src/Mundo/Methods.java
+++ ListasArreglos/src/Mundo/Methods.java
@@ -10,23 +10,30 @@ public class Methods {
 
         if (pos >= 0 && pos < array.length) {
 
-            for (int i = 0; i < array.length; i++) {
+            if (array[pos] != null) {
 
-                if (array[i] == null) {
+                array[pos] = ele;
 
-                    array[i] = ele;
-                    
-                    break;
-        
+            } else {
+
+                for (int i = 0; i < array.length; i++) {
+
+                    if (array[i] == null) {
+
+                        array[i] = ele;
+
+                        break;
+
+                    }
+
                 }
-
             }
-        } else{
+        } else {
             throw new Exception("La posición ingresada es inválida. Por favor, ingresa una posición entre 0 y " + (array.length - 1));
         }
 
     }
-    
+
     public void add(String ele, String[] array) throws Exception {
 
         for (int i = 0; i < array.length; i++) {
@@ -34,9 +41,9 @@ public class Methods {
             if (array[i] == null) {
 
                 array[i] = ele;
-                
+
                 break;
-    
+
             }
 
             // Si se recorre todo el arreglo y no se encuentra una posición disponible, se lanza una excepción
